c215ea82486131ad3bb8b0f68f5b6576ed683a83
Justin Popa <justin.popa20@gmail.com>
2017-06-01 18:47:29 +0300

    Prima tema

--- app/Iustinian/iustinian.module.js
+++ app/Iustinian/iustinian.module.js
@@ -7,9 +7,17 @@
                 url: '/iustinian',
                 templateUrl: 'app/Iustinian/iustinian.template.html'
             });
-            $stateProvider.state('iustinian.teme.dynamicTable', {
-                url: '/iustinian/teme/dynamicTable',
-                templateUrl: 'app/Iustinian/teme/dynamicTable/dynamicTable.template.html'
+            $stateProvider.state('iustinian.tema1', {
+                url: '/tema1',
+                templateUrl: 'app/Iustinian/teme/tema1/tema1.template.html'
+            });
+            $stateProvider.state('iustinian.tema2', {
+                url: '/tema2',
+                templateUrl: 'app/Iustinian/teme/tema2/tema2.template.html'
+            });
+            $stateProvider.state('iustinian.tema3', {
+                url: '/tema3',
+                templateUrl: 'app/Iustinian/teme/tema3/tema3.template.html'
             });
         }])
 }).call(this, this.angular);
